75178bb7dc31bca1e19871bf00cb5693f8d90766
samson <samson.chee@gmail.com>
2020-01-13 20:52:35 -0800

    add chinook demo db

--- sql_loader.js
+++ sql_loader.js
@@ -30,8 +30,42 @@ class sql_loader {
     const res = await this._db.query(query);
     for (let i = 0; i < res.rows.length; i++) {
       let e = res.rows[i].fqtn;
-      console.log(`dropping ${e}`);
+      console.log(`dropping table ${e}`);
       await this._db.query(`DROP TABLE ${e} CASCADE`);
+    }
+  }
+
+  async _dropAllSequences() {
+    const query = `
+      SELECT quote_ident(relname) AS seq
+      FROM   pg_class
+      WHERE  relkind = 'S'
+    `;
+
+    const res = await this._db.query(query);
+    for (let i = 0; i < res.rows.length; i++) {
+      let e = res.rows[i].seq;
+      console.log(`dropping seq ${e}`);
+      await this._db.query(`DROP SEQUENCE IF EXISTS ${e} CASCADE`);
+    }
+  }
+
+  async _dropAllFunctions() {
+    const query = `
+      SELECT quote_ident(pp.proname) as fn
+        FROM pg_proc pp
+       INNER JOIN pg_namespace pn on (pp.pronamespace = pn.oid)
+       INNER JOIN pg_language pl on (pp.prolang = pl.oid)
+       WHERE pp.proname NOT LIKE '_pg_%'
+         AND pl.lanname NOT IN ('c','internal')
+         AND pn.nspname NOT LIKE 'pg_%'
+    `
+
+    const res = await this._db.query(query);
+    for (let i = 0; i < res.rows.length; i++) {
+      let e = res.rows[i].fn;
+      console.log(`dropping function ${e}`);
+      await this._db.query(`DROP FUNCTION IF EXISTS ${e} CASCADE`);
     }
   }
 
@@ -50,6 +84,8 @@ class sql_loader {
       this._init();
 
       await this._dropAllTables();
+      await this._dropAllSequences();
+      await this._dropAllFunctions();
       await this._load(absPath);
 
       this._end();
